859785f67ed98d3d517114810f74cb57330d9876
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-02-14 23:20:53 +0100

    Package aangemaakt met daar in de Venue klassen. VenueController klasse aangemaakt in package van controller

---
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -0,0 +1,18 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Venue;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VenueController {
+
+    @GetMapping("/venuedetails")
+    public String venuedetails(Model model) {
+        Venue v = new Venue("Boesjkammeree", "https://abdellahdahmani.be/");
+        model.addAttribute("venue", v);
+        return "venuedetails";
+    }
+
+}
