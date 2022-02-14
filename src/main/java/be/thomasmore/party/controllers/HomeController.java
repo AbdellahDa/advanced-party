019feabb9f22cd6f82093f8a5097ccf187324517
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-02-14 17:31:35 +0100

    wijzigingen in about.html, home.html en pay toegevoegd.

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
     public String home(Model model) {
         int myCalculatedValue = 34 * 62;
         model.addAttribute("myCalculatedValue", myCalculatedValue);
-        model.addAttribute("appName","MyApp");
+        model.addAttribute("appName", "MyApp");
         return "home";
     }
+
     @GetMapping("/about")
     public String about(Model model) {
         String myName = "Abdellah";
@@ -22,5 +26,19 @@ public class HomeController {
         String myStreet = "Appelstraat 47";
         model.addAttribute("myStreet", myStreet);
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Before Formatting: " + now);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formatDateTime = now.format(format);
+        System.out.println("After Formatting: " + formatDateTime);
+        model.addAttribute("myDateNow",formatDateTime);
+        LocalDateTime later = now.plusDays(30);
+        String timeToPay = later.format(format);
+        model.addAttribute("myDateLater", timeToPay);
+        return "pay";
     }
 }
