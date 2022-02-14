2ff723b4a47be06edfa9e98f6a3c0aed19154f8f
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-02-14 16:43:33 +0100

    about.html, home.html en party.css toegevoegd

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,16 +1,25 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        String myName = "Abdellah";
+        model.addAttribute("myName", myName);
+        String myCity = "Borgerhout";
+        model.addAttribute("myCity", myCity);
+        String myStreet = "Appelstraat 47";
+        model.addAttribute("myStreet", myStreet);
         return "about";
     }
 }
