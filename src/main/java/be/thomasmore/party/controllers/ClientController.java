350bb7c48aafc79bd37e170ac5b98e34024d7709
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-02-19 15:08:41 +0100

    aanpassingen gemaakt in ClientController, de eerste twee letters van de naam, dan de laatste letter van de naam toegevoegd aan de methode generateClientCode

--- src/main/java/be/thomasmore/party/controllers/ClientController.java
+++ src/main/java/be/thomasmore/party/controllers/ClientController.java
@@ -1,5 +1,26 @@
 package be.thomasmore.party.controllers;
 
-public class ClientController {
+import be.thomasmore.party.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class ClientController {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public String generateClientCode() {
+        String firstName = "Pe";
+        String lastName  = "s";
+        String result = firstName + lastName;
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String firstName = "Pe";
+        String lastName  = "s";
+        String result = firstName + lastName;
+        System.out.println(result);
+    }
 }
