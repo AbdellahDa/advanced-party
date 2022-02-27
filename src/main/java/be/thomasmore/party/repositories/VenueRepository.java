720037d0ba6e955946eb80f4e9a3bf1f69d7676c
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-02-27 20:55:29 +0100

    errorpage toegevoegd

--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -3,6 +3,9 @@ package be.thomasmore.party.repositories;
 import be.thomasmore.party.model.Venue;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
     Iterable<Venue> findByOutdoor(boolean isOutdoor);
 
@@ -11,5 +14,13 @@ public interface VenueRepository extends CrudRepository<Venue, Integer> {
     Iterable<Venue> findByCapacityIsBetween(int i, int i1);
 
     Iterable<Venue> findByCapacityIsGreaterThan(int i);
+
+    Optional<Venue> findFirstByIdLessThanOrderByIdDesc(Integer id);
+
+    Optional<Venue> findFirstByIdGreaterThanOrderById(Integer id);
+
+    List<Object> findFirstByOrderByIdDesc();
+
+    List<Object> findFirstByOrderByIdAsc();
 
 }
