37699358e80df3e40ddf109bdf1a5a7857e91b5f
AbdellahDahmani <abdellah.dahmani@student.thomasmore.be>
2022-03-21 14:07:30 +0100

    VenueRepository aangepast

--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -5,38 +5,32 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
-    Iterable<Venue> findByOutdoor(boolean isOutdoor);
-    Iterable<Venue> findByIndoor(boolean isIndoor);
-
-    @Query("SELECT v FROM Venue v WHERE v.capacity <= :max")
-    Iterable<Venue> findCapacitySmallerThan(@Param("max") int capacity);
-    Iterable<Venue> findByCapacityLessThanEqual(int capacity);
-
-    @Query("SELECT v FROM Venue v WHERE v.capacity <= ?2 AND v.capacity >= ?1")
-    Iterable<Venue> findCapacityBetween(int min, int max);
-    Iterable<Venue> findByCapacityIsBetween(int min, int max);
-
-    @Query("SELECT v FROM Venue v WHERE v.capacity >= ?1")
-    Iterable<Venue> findCapacityBiggerThan(int capacity);
-    Iterable<Venue> findByCapacityGreaterThanEqual(int capacity);
-
     Optional<Venue> findFirstByIdLessThanOrderByIdDesc(int id);
     Optional<Venue> findFirstByIdGreaterThanOrderById(int id);
     Optional<Venue> findFirstByOrderByIdDesc();
     Optional<Venue> findFirstByOrderByIdAsc();
 
-    @Query("SELECT v FROM Venue v")
-    Iterable<Venue> findAllVenues();
+    @Query("SELECT v FROM Venue v WHERE (:min IS NULL OR v.capacity >= :min) AND (:max IS NULL OR v.capacity <= :max)")
+    List<Venue> findByCapacity(@Param("min") Integer min, @Param("max") Integer max);
+    // ==
+    @Query("SELECT v FROM Venue v WHERE (:min IS NULL OR v.capacity >= :min) AND (:max IS NULL OR v.capacity <= :max)")
+    Iterable<Venue> findByCapacityBetweenQuery(@Param("min") Integer min, @Param("max") Integer max);
 
-    @Query("SELECT v FROM Venue v WHERE (:min IS NULL OR v.capacity >= :min) AND " +
-            "(:max IS NULL OR v.capacity <= :max) AND " +
-            "(:distance IS NULL OR v.distanceFromPublicTransportInKm <= :distance) AND " +
-            "(:food IS NULL OR v.foodProvided = :food)")
-    Iterable<Venue> findVenues(@Param("min") Integer minimum,
-                               @Param("max") Integer maximum,
-                               @Param("distance") Double distance,
-                               @Param("food") Boolean food);
+    @Query("SELECT v FROM Venue v " +
+            "WHERE (:min IS NULL OR v.capacity >= :min) " +
+            "AND (:max IS NULL OR v.capacity <= :max) " +
+            "AND (:distance IS NULL OR v.distanceFromPublicTransportInKm <= :distance) " +
+            "AND (:food IS NULL OR v.foodProvided = :food) " +
+            "AND (:indoor IS NULL OR v.indoor = :indoor) " +
+            "AND (:outdoor IS NULL OR v.outdoor = :outdoor) ")
+    List<Venue> findByCapacityDistanceFoodIndoorOutdoor(
+            @Param("min") Integer min, @Param("max") Integer max,
+            @Param("distance") Double distance,
+            @Param("food") Boolean food,
+            @Param("indoor") Boolean indoor,
+            @Param("outdoor") Boolean outdoor);
 }
